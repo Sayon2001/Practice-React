62f06dafc4ba7027d1b7f278f125ff86a3d97dca
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-08-29 23:51:44 +0545

    added functionality

--- 3-calculator/src/App.jsx
+++ 3-calculator/src/App.jsx
@@ -1,11 +1,25 @@
 import styles from './App.module.css'
 import Display from './components/Display'
 import Buttons from './components/Buttons'
+import { useState } from 'react'
 function App() {
+
+  const [calValue, setCalVal] = useState("")
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("")
+    } else if (buttonText === "=") {
+      const result = eval(calValue)
+      setCalVal(result.toString())
+    } else {
+      const newDisplayedValue = calValue + buttonText
+      setCalVal(newDisplayedValue)
+    }
+  }
   return (<>
     <div className={styles.calculator}>
-      <Display />
-      <Buttons />
+      <Display displayValue={calValue} ></Display>
+      <Buttons onButtonClick={onButtonClick}></Buttons>
     </div>
   </>
   )
--- 3-calculator/src/components/Buttons.jsx
+++ 3-calculator/src/components/Buttons.jsx
@@ -1,13 +1,14 @@
 import styles from './Buttons.module.css'
 
-const Buttons = () => {
+const Buttons = ({ onButtonClick }) => {
     const buttonNames = ['7', '8', '9', 'C', '4', '5', '6', '/', '1', '2', '3', '-', '0', '.', '=', '+']
     return (
         <div className={styles.buttonContainer}>
-            {buttonNames.map((item) =>
-                <button className={styles.button} key={item}>{item}</button>
-            )}
-        </div>
+            {buttonNames.map((buttonName) =>
+                <button className={styles.button} key={buttonName} onClick={() => onButtonClick(buttonName)} > {buttonName}</button>
+            )
+            }
+        </div >
     )
 }
 

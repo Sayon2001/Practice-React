f442fcf3607bb4a88fe70458f0a7f95371ef1c50
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-09-16 22:50:49 +0545

    get blog api

--- reactBlogMS/src/pages/blog/Home.jsx
+++ reactBlogMS/src/pages/blog/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 import Card from './components/Card'
+import axios from 'axios'
+import { baseUrl } from '../../config'
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([])
+    const fetchBlogs = async () => {
+        const response = await axios.get(`${baseUrl}/blog`)
+        console.log(response)
+        if (response.status === 200) {
+            setBlogs(response.data.data)
+        }
+
+    }
+    useEffect(() => {
+        fetchBlogs()
+    }, [])
     return (
         <Layout>
             <div className='flex flex-wrap justify-center space-x-5 mt-6'>
